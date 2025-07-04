e8db46875a9ee4c21a1a1adebb14d86a6a23f5da
Gabrooh1 <gabrielesansonini1604@gmail.com>
2025-07-04 17:11:06 +0200

    porco

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { IonContent, IonCard, IonCardContent, IonButton, IonCardHeader, IonCardTitle } from '@ionic/angular/standalone';
+
 @Component({
   selector: 'app-page-not-found',
-  imports: [RouterLink],
+  imports: [RouterLink,
+    IonContent,
+    IonCard, 
+    IonCardContent, 
+    IonButton, 
+    IonCardHeader, 
+    IonCardTitle, 
+  ],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss'
 })
