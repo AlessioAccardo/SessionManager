47be1480978cbeb506e0cd8a3dab6d904ad2c424
Gabrooh1 <gabrielesansonini1604@gmail.com>
2025-07-09 12:50:05 +0200

    bo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
 // app.component.ts
 
 import { Component, inject, OnInit } from '@angular/core';
-import { IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet,IonMenu, IonHeader, IonSplitPane, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Platform } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './services/auth/auth.service';
 import { Observable } from 'rxjs';
 import { LoggedUser } from './interfaces/loggedUser.interface';
-
+import { TabsPage } from './tabs/tabs.component';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrl: 'app.component.scss',
-  imports: [CommonModule, IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonContent, NavBarComponent],
+  imports: [CommonModule, TabsPage, IonApp, IonRouterOutlet, IonMenu, IonSplitPane, IonHeader, IonToolbar, IonTitle, IonContent, NavBarComponent],
   standalone: true,
 })
 export class AppComponent implements OnInit {
